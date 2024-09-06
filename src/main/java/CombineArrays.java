59aa35cb00d90bce6edb1645afb5e0baf7d35524
Carlus Brown <carlusbrown@gmail.com>
2024-09-06 00:53:54 +0000

    Completed CombineArrays Lab

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int leftover = 0;
+        int[] arr3 = new int[arr1.length + arr2.length];
+        
+        for(int i : arr1){
+            arr3[leftover] = i;
+            leftover++;
+        }
+        for(int i : arr2){
+            arr3[leftover] = i;
+            leftover++;
+        }
 
-        return null;
+        return arr3;
     }
 }
